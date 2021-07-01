7b000dc815e642c51c6b5161ca4a3a926fc27344
lcw57 <dlcjfdnr789@naver.com>
2021-07-01 17:07:26 +0900

    7/1 2ban class commit 1

---
+++ accountapp/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def hello_world (request):
+    # 라우팅 해줘야해 (연결)
+    # 메인 앱에서 먼저 받고 여기로 보냄
+
+
+    return HttpResponse("Hello world!")
+
