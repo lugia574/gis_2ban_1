2284bd5ced836cecaf133287b3bf5421d82e5a18
lcw57 <dlcjfdnr789@naver.com>
2021-07-08 17:13:25 +0900

    get// post 2021/07/08 - 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -8,6 +8,7 @@ def hello_world (request):
     # 라우팅 해줘야해 (연결)
     # 메인 앱에서 먼저 받고 여기로 보냄
 
-
-    return render(request, 'accountapp/hello_world.html')
-
+    if request.method == 'POST': # 기본적으로 겟 방식으로 작동함
+        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD!'})
+    else:
+        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!'})
