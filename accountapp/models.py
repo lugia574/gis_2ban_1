f5d0ade36a119e3efe8870abef6a6a235193f532
lcw57 <dlcjfdnr789@naver.com>
2021-07-08 18:14:54 +0900

    DB 2021/07/08 - 4

--- accountapp/models.py
+++ accountapp/models.py
@@ -5,6 +5,7 @@ class HelloWorld(models.Model):
     text = models.CharField(max_length=255, null= False) # 문자열 데이터를 여기다가 박을꺼야
 
 
+
 # python manage.py migrate 이걸 쳐야해
 # python manage.py makemigrations
 #
--- accountapp/views.py
+++ accountapp/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import HelloWorld
 
 
 def hello_world (request):
@@ -9,6 +10,13 @@ def hello_world (request):
     # 메인 앱에서 먼저 받고 여기로 보냄
 
     if request.method == 'POST': # 기본적으로 겟 방식으로 작동함
-        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD!'})
+        temp = request.POST.get('input')
+
+        new_data = HelloWorld()
+        new_data.text = temp
+        new_data.save()
+
+
+        return render(request, 'accountapp/hello_world.html', context={'text': new_data})
     else:
         return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!'})
